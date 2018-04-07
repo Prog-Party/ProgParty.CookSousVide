855eef99dfcb128bb610854d2146316e41c22949
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2018-04-07 02:08:02 +0200

    Fixed the layout

--- ProgParty.CookSousVide/Controllers/HomeController.cs
+++ ProgParty.CookSousVide/Controllers/HomeController.cs
@@ -20,23 +20,7 @@ namespace ProgParty.CookSousVide.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
